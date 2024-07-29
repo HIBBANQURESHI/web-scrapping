2b2a2ec9fe32755c77e2d10b1e06ee158d6c91bf
HIBBANQURESHI <156018725+HIBBANQURESHI@users.noreply.github.com>
2024-07-29 23:50:28 +0500

    add

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-req = requests.get('https://codesignservices.com/contact')
+req = requests.get('https://x.com/search?q=jobs&src=typed_query')
 
+# Parse the HTML content
 soup = BeautifulSoup(req.content, "html.parser")
 
-name_field = soup.find('input', {'name': 'hibban'})
-email_field = soup.find('input', {'name': 'hibban@gmail.com'})
-message_field = soup.find('textarea', {'name': 'scrapper'})
+spans = soup.find_all('span', class_='css-1jxf684')
 
-name = name_field.get('value', 'No name field found') if name_field else 'No name field found'
-email = email_field.get('value', 'No email field found') if email_field else 'No email field found'
-message = message_field.get_text(strip=True) if message_field else 'No message field found'
-
-print(f'Name: {name}')  
-print(f'Email: {email}')
-print(f'Message: {message}')
+for span in spans:
+    text = span.get_text(strip=True)
+    print(text)
