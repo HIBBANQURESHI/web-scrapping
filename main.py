9357995a159b701b50f2712bfccc2c3113733563
HIBBANQURESHI <156018725+HIBBANQURESHI@users.noreply.github.com>
2024-07-30 02:39:34 +0500

    implementing user agents

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pandas as pd
 from ntscraper import Nitter
+import requests
+import random
 
-scraper = Nitter()
+def agent(username, mode, number):
+    scraper = Nitter()
 
-tweets = scraper.get_tweets('Web3Career', mode = 'user', number = 5)
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/8'
+    ]
 
-for tweet in tweets ['tweets']:
-    print(tweet)
-    print('-------- Next Tweet --------')
+    tweets = scraper.get_tweets(username, mode=mode, number=number)
+
+    for tweet in tweets['tweets']:
+        random_ua = random.choice(user_agents)
+
+        scraper.session = requests.Session()
+        scraper.session.headers['User-Agent'] = random_ua
+
+        print(tweet)
+        print('-------- Next Tweet --------')
+        print(agent)
+        print(user_agents)
+
+tweets = agent('Web3Career', mode='user', number=5)
